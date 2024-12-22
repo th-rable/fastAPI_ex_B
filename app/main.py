58cc361d47a48931ac8478b3a67e6bd3012baef7
rable <73579842+th-rable@users.noreply.github.com>
2024-12-22 07:12:09 +0000

    add cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from routers import items, users, socket
 from sockets import sio_app
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(users.router)
 app.include_router(items.router)
 app.include_router(socket.router)
