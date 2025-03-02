1117e874d5f535a35542129c43600ff071eb1b98
rable <73579842+th-rable@users.noreply.github.com>
2025-03-02 03:53:30 +0000

    test

--- app/main.py
+++ app/main.py
@@ -6,12 +6,12 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins={
-    "http://localhost:3000"
+    "https://snsapp.pages.dev"
 }
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
