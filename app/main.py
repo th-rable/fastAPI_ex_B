d75f370b21786e9d560c645cf9d04eda1fa78b73
rable <73579842+th-rable@users.noreply.github.com>
2025-03-02 03:39:29 +0000

    test

--- app/main.py
+++ app/main.py
@@ -5,12 +5,16 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins={
+    "https://localhost:3000"
+}
+
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=["*"],
-    # allow_credentials=True,
-    # allow_methods=["*"],
-    # allow_headers=["*"],
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(users.router)
